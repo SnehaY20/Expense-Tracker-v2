0f00538f122a7c6eaa197e542cd43e306f3c8940
SnehaY20 <21snehay@gmail.com>
2024-10-29 17:17:03 +0530

    added env variables and initialized the server.

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load env variables
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Sneha here!</h1>");
+});
+
+app.listen(PORT, () => {
+  PORT,
+    console.log(
+      `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    );
+});
